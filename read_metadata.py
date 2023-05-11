b978c0e88cfc8cb8de1c291d70ffbfdacf3b5cd7
anthony_nobara <anthonynmui@gmail.com>
2023-05-11 15:42:16 -0400

    added read metadata file

---
+++ read_metadata.py
@@ -0,0 +1,13 @@
+# import csv
+#
+# with open('./Project_CodeNet/metadata/p00000.csv', mode = 'r') as csvfile:
+#     spamreader = csv.DictReader(csvfile, delimiter=' ', quotechar='|')
+#     for row in spamreader:
+#         print(row)
+
+import pandas
+# reading the CSV file
+csvFile = pandas.read_csv('./Project_CodeNet/metadata/p00000.csv')
+# displaying the contents of the CSV file
+# print(csvFile["problem_id"])
+print(csvFile["submission_id"])
